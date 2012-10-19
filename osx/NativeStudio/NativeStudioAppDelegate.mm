27571fe1ec4ac9cf9a2c1d5365a41ca213c2db1a
Anthony Catel <a.catel@weelya.com>
2012-10-19 14:16:50 +0300

    Update osx/NativeStudio/NativeStudioAppDelegate.mm

--- osx/NativeStudio/NativeStudioAppDelegate.mm
+++ osx/NativeStudio/NativeStudioAppDelegate.mm
@@ -1,13 +1,15 @@
 #import "NativeStudioAppDelegate.h"
-#import <OpenGL/gl.h>
+#import <OpenGL/gl3.h>
 #import <SDL2/SDL.h>
-//#import <SDL2/SDL_opengl.h>
+#import <SDL2/SDL_opengl.h>
 #import <SDL2/SDL_video.h>
-
+#import <pthread.h>
 
 #import "NativeJS.h"
 #import "NativeSkia.h"
 #import "NativeConsole.h"
+#import "native_netlib.h"
+#import "ape_timers.h"
 
 #define kNativeWidth 1024
 #define kNativeHeight 768
@@ -15,10 +17,16 @@
 
 
 uint32_t tfps = 0, ttfps = 0;
+int ape_running = 1;
 
 NativeJS *NJS;
+ape_global *gnet = NULL;
 NativeConsole *console;
+int NativeEvents(SDL_Window *win);
 
+uint32_t starttime = SDL_GetTicks();
+uint32_t newtime;
+unsigned int fboTex, fboHandle;
 
 @implementation NativeStudioAppDelegate
 
@@ -52,97 +60,44 @@ Uint32 NativeFPS(Uint32 interval,
     return 1000;
 }
 
-#define NUM_SOUNDS 1
-struct sample {
-    Uint8 *data;
-    Uint32 dpos;
-    Uint32 dlen;
-} sounds[NUM_SOUNDS];
 
-void mixaudio(void *unused, Uint8 *stream, int len)
+
+static int NativeProcessUI(void *arg)
 {
-    int i;
-    Uint32 amount;
-    
-    memset(stream, 0, len);
-
-        amount = (sounds[i].dlen-sounds[i].dpos);
-        if ( amount > len ) {
-            amount = len;
-        }
-        
-        memcpy(stream, &sounds[i].data[sounds[i].dpos], amount);
-        //SDL_MixAudio(stream, &sounds[i].data[sounds[i].dpos], amount, SDL_MIX_MAXVOLUME);
-        sounds[i].dpos += amount;
-    SDL_Event user_event;
-    
-    int16_t *jsbuffer = (int16_t *)malloc(sizeof(int16_t) * len);
-    memcpy(jsbuffer, &sounds[i].data[sounds[i].dpos], amount/2);
-    
-    user_event.type=SDL_USEREVENT;
-    user_event.user.code=42;
-    user_event.user.data1=jsbuffer;
-    user_event.user.data2=NULL;
-    SDL_PushEvent(&user_event);
-
+    SDL_Window *win = (SDL_Window *)arg;
+    //printf("process UI\n");
+    return NativeEvents(win);
 }
 
-void PlaySound(char *file)
+static void NativeRunMainLoop(ape_global *net, SDL_Window *win)
 {
-    int index;
-    SDL_AudioSpec wave;
-    Uint8 *data;
-    Uint32 dlen;
-    SDL_AudioCVT cvt;
+    add_timer(&net->timersng, 1, NativeProcessUI, (void *)win);
     
-    /* Look for an empty (or finished) sound slot */
-    for ( index=0; index<NUM_SOUNDS; ++index ) {
-        if ( sounds[index].dpos == sounds[index].dlen ) {
-            break;
-        }
-    }
-    if ( index == NUM_SOUNDS )
-        return;
-    
-    /* Load the sound file and convert it to 16-bit stereo at 22kHz */
-    if ( SDL_LoadWAV(file, &wave, &data, &dlen) == NULL ) {
-        fprintf(stderr, "Couldn't load %s: %s\n", file, SDL_GetError());
-        return;
-    }
-    SDL_BuildAudioCVT(&cvt, wave.format, wave.channels, wave.freq,
-                      AUDIO_S16,   2,             44100);
-    cvt.buf = (Uint8 *)malloc(dlen*cvt.len_mult);
-    memcpy(cvt.buf, data, dlen);
-    cvt.len = dlen;
-    SDL_ConvertAudio(&cvt);
-    SDL_FreeWAV(data);
-    
-    /* Put the sound data in the slot (it starts playing immediately) */
-    if ( sounds[index].data ) {
-        free(sounds[index].data);
-    }
-    SDL_LockAudio();
-    sounds[index].data = cvt.buf;
-    sounds[index].dlen = cvt.len_cvt;
-    sounds[index].dpos = 0;
-    SDL_UnlockAudio();
+    events_loop(net);
 }
 
-void NativeEvents(SDL_Window *win)
+
+int NativeEvents(SDL_Window *win)
 {   
-    uint32_t starttime = SDL_GetTicks();
-    uint32_t newtime;
-    SDL_Event event;
 
-    while(1) {
-        
+    SDL_Event event;
+    uint32_t tstart, tend;
+    tstart = SDL_GetTicks();
+    //while(1) {
+    int nevents = 0;
         while(SDL_PollEvent(&event)) {
+            nevents++;
             switch(event.type) {
+                case SDL_TEXTINPUT:
+                    NJS->textInput(event.text.text);
+                    break;
                 case SDL_USEREVENT:
                     //NJS->bufferSound((int16_t *)event.user.data1, 4096);
                     break;
                 case SDL_QUIT:
-                    return;
+                    ape_running = 0;
+                    delete NJS;
+                    return 0;
                 case SDL_MOUSEMOTION:
                     NJS->mouseMove(event.motion.x, event.motion.y,
                                    event.motion.xrel, event.motion.yrel);
@@ -160,8 +115,14 @@ void NativeEvents(SDL_Window *win)
                                     event.button.state, event.button.button);
                 break;
                 case SDL_KEYDOWN:
+                case SDL_KEYUP:
+                {
+                    int keyCode = 0;
+                    
+                    int mod = 0;
                     if (
-                        (&event.key)->keysym.sym == SDLK_r) {
+                        (&event.key)->keysym.sym == SDLK_r &&
+                        event.key.keysym.mod & KMOD_GUI) {
                         //printf("Refresh...\n");
                         [console clear];
                         delete NJS;
@@ -171,49 +132,69 @@ void NativeEvents(SDL_Window *win)
                         
                         NJS = new NativeJS();
                         NJS->nskia->bindGL(kNativeWidth, kNativeHeight);
+                        NJS->bindNetObject(gnet);
                         NJS->LoadScript("./main.js");
                         //SDL_GL_SwapBuffers();
-                        
-                        
+                        break;
                     }
+                    if (event.key.keysym.sym >= 97 && event.key.keysym.sym <= 122) {
+                        keyCode = event.key.keysym.sym - 32;
+                    } else {
+                        keyCode = event.key.keysym.sym;
+                    }
+                    
+                    if (event.key.keysym.mod & KMOD_SHIFT) {
+                        mod |= NATIVE_KEY_SHIFT;
+                    }
+                    if (event.key.keysym.mod & KMOD_ALT) {
+                        mod |= NATIVE_KEY_ALT;
+                    }
+                    if (event.key.keysym.mod & KMOD_CTRL) {
+                        mod |= NATIVE_KEY_CTRL;
+                    }
+                    
+                    NJS->keyupdown(keyCode, mod, event.key.state, event.key.repeat);
+                    /*printf("Mapped to %d\n", keyCode);
+                    printf("Key : %d %d %d %d %d uni : %d\n", event.key.keysym.sym,
+                           event.key.repeat,
+                           event.key.state,
+                           event.key.type,
+                           event.key.keysym.scancode,
+                           event.key.keysym.unicode);*/
                     //return;
                     break;
-
+                }
             }
         }
         
-        //glClear( GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
         //NJS->nskia->redrawScreen();
         /*glClear (GL_STENCIL_BUFFER_BIT);
 
         glEnable(GL_STENCIL_TEST) ;
         glStencilFunc(GL_ALWAYS, 1, 1);
         glStencilOp(GL_REPLACE, GL_REPLACE, GL_REPLACE);*/
+        
+
+//        glDisable(GL_ALPHA_TEST);
+
         NJS->callFrame();
         NJS->nskia->flush();
-        //glDisable(GL_STENCIL_TEST);
-        glFlush();
-        //glFinish();
-        //glDrawBuffer(GL_FRONT);
-        //glDrawBuffer(GL_BACK);
-        //SDL_GL_SwapBuffers();
-        //SDL_GL_SwapWindow(win);
-        if (++ttfps == 30) {
-            NJS->gc();
+
+        SDL_GL_SwapWindow(win);
+        //NJS->nskia->resetGLContext();
+
+        tend = SDL_GetTicks();
+    
+        if (tend - tstart > 10) {
+            //NSLog(@"Took : %d\n", tend - tstart);
+        //NJS->gc();
             ttfps = 0;
         }
-        //NJS->gc();
-        
-        newtime = SDL_GetTicks();
-        
-        if (newtime - starttime < 1000/kFPS) {
-            /* TODO: JS_GC() */
-            SDL_Delay((1000/kFPS) - (newtime - starttime));
-        }
-        
         tfps++;
-        starttime = SDL_GetTicks();
-    }
+    //}
+
+    //NSLog(@"ret : %d for %d events", (tend - tstart), nevents);
+    return 16;
 }
 
 
@@ -222,7 +203,7 @@ void resizeGLScene(int width, int height)
 {
     glViewport(0, 0, width, height);
     
-    
+#if 0    
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     
@@ -233,30 +214,95 @@ void resizeGLScene(int width, int height)
     glMatrixMode(GL_MODELVIEW);
     
     glLoadIdentity();
-    
+#endif    
 }
+
+void setupFBO()
+{
+    int fbo_width = kNativeWidth;
+    int fbo_height = kNativeHeight;
+    unsigned int depth;
+    
+    glEnable(GL_TEXTURE_2D);
+    
+    glGenFramebuffers(1, &fboHandle);
+    
+    glBindFramebuffer(GL_FRAMEBUFFER, fboHandle);
+    glGenTextures(1, &fboTex);
+    
+    glBindTexture(GL_TEXTURE_2D, fboTex);
+    glTexImage2D( GL_TEXTURE_2D,
+                 0,
+                 GL_RGBA,
+                 fbo_width, fbo_height,
+                 0,
+                 GL_RGBA,
+                 GL_UNSIGNED_BYTE,
+                 NULL);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, fboTex, 0);
+    
+    glGenRenderbuffers(1, &depth);
+    
+    //glBindRenderbuffer(GL_RENDERBUFFER, depth);
+    //glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, fbo_width, fbo_height);
+    //glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depth);
+    // FBO status check
+    GLenum status;
+    status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+    switch(status) {
+        case GL_FRAMEBUFFER_COMPLETE:
+            printf("fbo complete %d\n", fboHandle);
+            break;
+            
+        case GL_FRAMEBUFFER_UNSUPPORTED:
+            printf("fbo unsupported\n");
+            break;
+            
+        default:
+            /* programming error; will fail on all hardware */
+            printf("fbo error\n");
+            break;
+    }
+    glBindTexture(GL_TEXTURE_2D, 0);
+    //glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glDisable(GL_TEXTURE_2D);
+}
+
+
 int initGL()
 {
     //glShadeModel(GL_SMOOTH);
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glHint(GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
-    glEnable(GL_TEXTURE_2D);
-    glClearColor(1, 1, 1, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glHint(GL_MULTISAMPLE_FILTER_HINT_NV, GL_FASTEST);
-    glEnable(GL_MULTISAMPLE);
+    //glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    //glEnable(GL_BLEND);
+    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    //glHint(GL_LINE_SMOOTH_HINT, GL_DONT_CARE);
+    //glEnable(GL_TEXTURE_2D);
+    //glClearColor(1, 1, 1, 0);
+    //glClear(GL_COLOR_BUFFER_BIT);
+    //glHint(GL_MULTISAMPLE_FILTER_HINT_NV, GL_FASTEST);
+    //glEnable(GL_MULTISAMPLE);
     //glClearDepth(1.0f);
     //glEnable(GL_DEPTH_TEST);
     //glDepthFunc(GL_LEQUAL);    
     //glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    //glBindFramebuffer(GL_FRAMEBUFFER, val);
+    //glDrawBuffer(GL_COLOR_ATTACHMENT0_EXT);
+
+    //glViewport(0,0,1024,768);
+    
+    //setupFBO();
     
     return 1;
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    
+    [self.window close];
     SDL_Window *win;
     SDL_GLContext contexteOpenGL;
     
@@ -269,19 +315,18 @@ int initGL()
         return;
     }
 
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    
+    //SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
     
     //SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, true );
     //SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, 4 ); /* TODO check */
-    //SDL_GL_SetSwapInterval(1);
+
     //SDL_GL_SetAttribute(SDL_GL_SWAP_CONTROL, 0);
     SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5 );
     SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5 );
     SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5 );
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8 );
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0 );
     SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32 );
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 0 );
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1 );
     
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
@@ -291,51 +336,61 @@ int initGL()
     win = SDL_CreateWindow("Native - Running", 100, 100, kNativeWidth, kNativeHeight, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
     contexteOpenGL = SDL_GL_CreateContext(win);
-
+    if (SDL_GL_SetSwapInterval(1) == -1) {
+        printf("Cant vsync\n");
+    }
     //SDL_SetWindowFullscreen(win, SDL_TRUE);
 
     resizeGLScene(kNativeWidth, kNativeHeight);
+    glClearColor(0, 0, 0, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    SDL_GL_SwapWindow(win);
+    //sleep(10000);
     initGL();
-    
+
     NJS->nskia->bindGL(kNativeWidth, kNativeHeight);
+    //NJS->nskia->drawRect(0, 0, 10, 10, 0);
+
     
+    int e = 0;
+    if ((e = glGetError()) != GL_NO_ERROR) {
+        printf("first error %d\n", e);
+    }
     [self setupWorkingDirectory:YES];
     
     SDL_AddTimer(1000, NativeFPS, NULL);
+    SDL_StartTextInput();
     
-    SDL_AudioSpec desired, obtained, soundfile;
-    SDL_AudioSpec fmt;
+    gnet = native_netlib_init();
     
-    /* Un son sereo de 16 bits à 22kHz */
-    fmt.freq = 44100;
-    fmt.format = AUDIO_S16;
-    fmt.channels = 2;
-    fmt.samples = 4096;        /*Une bonne valeur pour les jeux */
-    fmt.callback = mixaudio;
-    fmt.userdata = NULL;
-    
-    /* Ouvre le contexte audio et joue le son */
-    if ( SDL_OpenAudio(&fmt, NULL) < 0 ) {
-        fprintf(stderr, "Impossible d'accéder à l'audio: %s\n", SDL_GetError());
-        exit(1);
-    }
+    NJS->bindNetObject(gnet);
 
-    PlaySound("/Users/anthonycatel/pof2.wav");
     
-    //SDL_PauseAudio(0);
     if (!NJS->LoadScript("./main.js")) {
         NSLog(@"Cant load script");
     }
 
+    e = 0;
+    if ((e = glGetError()) != GL_NO_ERROR) {
+        printf("got an error here %d\n", e);
+    }    
     printf("[DEBUG] OpenGL %s\n", glGetString(GL_VERSION));
     atexit( SDL_Quit );
     //glClear(GL_COLOR_BUFFER_BIT);
-    NJS->nskia->flush();
-    SDL_GL_SwapWindow(win);
-    
-    //[self.window dealloc];
-    NativeEvents(win);
+    //glPushAttrib(GL_ALL_ATTRIB_BITS);
 
+
+    e = 0;
+    if ((e = glGetError()) != GL_NO_ERROR) {
+        printf("got an error here %d\n", e);
+    }
+    SDL_GL_SwapWindow(win);
+    //glPixelStorei(GL_PACK_ALIGNMENT, 1);
+
+
+        //[self.window dealloc];
+    //NativeEvents(win);
+    NativeRunMainLoop(gnet, win);
 }
 
 @end
