a5602fc869756dc5b3f7149da617ece8870dd3e9
efyx <n.trani@weelya.com>
2012-10-22 19:26:24 +0200

    Adding gain demo

---
+++ UICore/demos/test.audioslide.js
@@ -0,0 +1,59 @@
+var main = new Application();
+
+var HTTPAudioRequest = function(url, cb) {
+    var r  = new Http(url).request(function(e) {
+        if (e.type == "audio") {
+            echo("its data");
+            cb(e.data);
+        } else {
+            echo("no data");
+        }
+    });
+}
+
+var slider3 = main.add("UISliderController", {
+    x : 350,
+    y : 240,
+    w : 250,
+    h : 20,
+
+    displayLabel : true,
+    labelBackground : 'rgba(255, 255, 255, 0.7)',
+    labelColor : 'rgba(0, 0, 0, 0.5)',
+    labelOffset : -18,
+    labelWidth : 56,
+    labelPrefix : 'f=',
+    labelSuffix : ' %',
+    fontSize : 10,
+    lineHeight : 18,
+
+    splitColor : 'rgba(0, 0, 0, 0.5)',
+    boxColor : 'rgba(255, 255, 255, 0.02)',
+
+    disabled : false,
+    radius : 2,
+    min : 0,
+    max : 5,
+    value : 0.5
+});
+
+HTTPAudioRequest("http://f.z.nf/song.mp3", function(data) {
+    source.open(data);
+    source.play();
+});
+
+var dsp = new Audio(1024, 2, 44100);
+
+var source = dsp.createNode("source", 0, 2);
+var gain = dsp.createNode("gain", 2, 2);
+var target = dsp.createNode("target", 2, 0);
+
+dsp.connect(source.output(0), gain.input(0));
+dsp.connect(source.output(1), gain.input(1));
+
+dsp.connect(gain.output(0), target.input(0));
+dsp.connect(gain.output(1), target.input(1));
+
+slider3.addEventListener("change", function(value){
+    gain.set("gain", value);
+}, false);
