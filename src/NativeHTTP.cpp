86db13ace0c26129da05d95114db84f861f56bb6
efyx <n.trani@weelya.com>
2012-11-26 15:42:56 +0100

    Adding wave format

--- src/NativeHTTP.cpp
+++ src/NativeHTTP.cpp
@@ -21,6 +21,7 @@ static struct native_http_mime {
     {"audio/mpeg",              NativeHTTP::DATA_AUDIO},
     {"audio/wave",              NativeHTTP::DATA_AUDIO},
     {"audio/ogg",               NativeHTTP::DATA_AUDIO},
+    {"audio/x-wav",             NativeHTTP::DATA_AUDIO},
     {"video/ogg",               NativeHTTP::DATA_AUDIO},
     {"audio/webm",              NativeHTTP::DATA_AUDIO},
     {"application/json",        NativeHTTP::DATA_JSON},
