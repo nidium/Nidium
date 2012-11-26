349e19af382cc3d895e0c1288ec3e71cce130ed7
efyx <n.trani@weelya.com>
2012-11-26 15:46:47 +0100

    Makefile and build for lysee

--- linux/build.sh
+++ linux/build.sh
@@ -22,14 +22,14 @@
 g++ \
     main.cpp \
     -fno-rtti -ffunction-sections -fdata-sections -fno-exceptions -DNDEBUG -DTRIMMED -g -O3 -freorder-blocks  -fomit-frame-pointer \
-    -I ../../SDL/SDL2/include/ \
+    -I ../SDL/include/ \
     -I ../src/ \
     -I ../network/ \
-    -I ../mozilla/js/src/dist/include/ \
-    -L ../mozilla/js/src/ \
-    -L ../../SDL/build/.libs/ \
+    -I /home/efyx/dev/mozilla/js/src/dist/include/ \
+    -L /home/efyx/dev/mozilla/js/src/ \
+    -L ../SDL/build/build/.libs/ \
     -L ../skia-read-only/out/Release/obj.target/gyp/ \
-    -L ../../../mozilla-central/js/src/ \
+    -L ../../../mozilla/js/src/ \
     -L ../src/ \
     -L ../network/ \
     -L ../audio/ \
@@ -37,10 +37,12 @@ g++ \
     -L ../../ffmpeg/libavcodec/ \
     -L ../../ffmpeg/libavutil/ \
     -L ../c-ares/.libs/ \
-    ../../SDL/build/.libs/libSDL2.a -lGL -lfreetype -lrt -lz -lpthread -lpng \
+    ../SDL/build/build/.libs/libSDL2.a -lGL -lfreetype -lrt -lz -lpthread -lpng \
     -Wl,--start-group \
+    -lnspr4 \
+    -ljs_static \
     -lnativeaudio \
     -lavformat -lavcodec -lavutil -lportaudio -lzita-resampler \
-    -lnativestudio -lnativenetwork -lcares -lskia_sfnt -ljs_static -lzlib -ljpeg -lskia_opts_ssse3 -lskia_opts -lskia_utils -lpicture_utils -lskia_ports -lskia_images -lskia_skgr -lskia_gr -lskia_effects -lskia_core -Wl,--end-group
+    -lnativestudio -lnativenetwork -lcares -lskia_sfnt -lzlib -ljpeg -lskia_opts_ssse3 -lskia_opts -lskia_utils -lpicture_utils -lskia_ports -lskia_images -lskia_skgr -lskia_gr -lskia_effects -lskia_core -Wl,--end-group
 
 cp a.out ../UICore/
