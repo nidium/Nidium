22c5be49055f073d981ba8c5b958284f8c20035a
efyx <n.trani@weelya.com>
2012-12-14 10:00:46 +0100

    Add GLSL translator to WebGL
    Additionally Makefile is updated with more consistent path to libs and you
    can disable WebGL easily

--- linux/build.sh
+++ linux/build.sh
@@ -12,9 +12,11 @@ g++ \
     -L ../src/ \
     -L ../network/ \
     -L ../c-ares/.libs/ \
+    -L ../third-party/angle/out/Debug/obj.target/src/ \
     ../SDL/build/build/.libs/libSDL2.a -lGL -lfreetype -lrt -lz -lpthread -lpng \
     -Wl,--start-group \
-    -ljs_static \
+    -ltranslator_glsl -ltranslator_common -lpreprocessor \
+    -ljs_static -lnspr4 \
     -lnativestudio -lnativenetwork -lcares -lskia_sfnt -lzlib -ljpeg -lskia_opts_ssse3 -lskia_opts -lskia_utils -lpicture_utils -lskia_ports -lskia_images -lskia_skgr -lskia_gr -lskia_effects -lskia_core \
     -Wl,--end-group
 
