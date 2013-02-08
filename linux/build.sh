78c4f991890bd8fa7eb6477d167250a80dfa5412
efyx <n.trani@weelya.com>
2013-02-08 10:00:23 +0100

    Fix ffmpeg path

--- linux/build.sh
+++ linux/build.sh
@@ -12,15 +12,16 @@ g++ \
     -L ../src/ \
     -L ../network/ \
     -L ../audio/ \
-    -L ../../ffmpeg/libavformat/ \
-    -L ../../ffmpeg/libavcodec/ \
-    -L ../../ffmpeg/libavutil/ \
+    -L ../third-party/ffmpeg/libavformat/ \
+    -L ../third-party/ffmpeg/libavcodec/ \
+    -L ../third-party/ffmpeg/libavutil/ \
+    -L ../third-party/ffmpeg/libswscale/ \
     -L ../c-ares/.libs/ \
     -L ../third-party/angle/out/Debug/obj.target/src/ \
     ../third-party/SDL/build/build/.libs/libSDL2.a -lGL -lfreetype -lrt -lz -lpthread -lpng \
     -Wl,--start-group \
     -lnativeaudio \
-    -lavformat -lavcodec -lavutil -lportaudio -lzita-resampler \
+    -lswscale -lavformat -lavcodec -lavutil -lportaudio -lzita-resampler \
     -ltranslator_glsl -ltranslator_common -lpreprocessor \
     -ljs_static -lnspr4 \
     -lnativestudio -lnativenetwork -lcares -lskia_sfnt -lzlib -ljpeg -lskia_opts_ssse3 -lskia_opts -lskia_utils -lpicture_utils -lskia_ports -lskia_images -lskia_skgr -lskia_gr -lskia_effects -lskia_core \
