60d9377c3ca8df7fce3dee6713d8eff590acaedf
eFyX <n.trani@weelya.com>
2013-03-04 13:56:20 +0100

    Use thrird-party gyp instead of system one

--- gyp/gyp.sh
+++ gyp/gyp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export CXX=/usr/bin/clang++
 export GYP_DEFINES="clang=1"
-gyp -d all --include=config.gypi --include=common.gypi --depth ./ all.gyp
+GYP=../third-party/gyp/gyp
+$GYP -d all --include=config.gypi --include=common.gypi --depth ./ all.gyp
