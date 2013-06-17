64c94383ee62241390b2ef5be7d3c3b8a4864608
paraboul <a.catel@weelya.com>
2013-06-17 12:36:01 +0200

    re adding main.js

---
+++ framework/main.js
@@ -0,0 +1,95 @@
+/* ------------------------+------------- */
+/* Native Framework 2.0    | Falcon Build */
+/* ------------------------+------------- */
+/* (c) 2013 Stight.com - Vincent Fontaine */
+/* -------------------------------------- */
+
+/* -- Falcon Framework ------------------ */
+load("../scripts/preload.js");
+load("falcon/native.js");
+
+//load("applications/demos/hello.js");
+//load("applications/demos/motion.js");
+//load("applications/demos/tabs.js");
+//load("applications/demos/profiler.js");
+//load("applications/demos/style.js");
+//load("applications/demos/windows.js");
+//load("applications/demos/dropdown.js");
+//load("applications/demos/sliders.js");
+//load("applications/demos/scrollbars.js");
+//load("applications/demos/modal.js");
+//load("applications/demos/threads.js");
+//load("applications/demos/tooltips.js");
+//load("applications/demos/animation.js"); // FAIL TO RUN
+
+/* media demos */
+//load("applications/media/video.js");
+
+//load("applications/audio/test.js");
+//load("applications/audio/mixer.js");
+//load("applications/audio/dsp.js");
+
+
+/* charts demos */
+//load("applications/charts/line.js");
+//load("applications/charts/pie.js");
+//load("applications/charts/polar.js");
+//load("applications/charts/donut.js");
+//load("applications/charts/radar.js");
+//load("applications/charts/bar.js");
+//load("applications/charts/demo.js");
+//load("applications/charts/line.js");
+
+
+/* Shaders Demos */
+//load("applications/demos/shader.js");
+//load("applications/demos/shader.basic.js");
+//load("applications/demos/shader.advanced.js");
+
+//load("applications/demos/splines.js");
+//load("applications/demos/diagrams.js");
+
+//load("applications/demos/flickr.js");
+//load("applications/demos/http.js");
+
+//load("applications/demos/buttons.js");
+//load("applications/demos/text.js");
+//load("applications/demos/test.js");
+
+//load("testLayer.js");
+
+
+/* File API Test */
+//load("applications/demos/file.basic.js");
+//load("applications/demos/file.advanced.js");
+
+/* CommonJS */
+// CommonJS Tests
+//load("commonjs/run.js");
+// Native module test
+/*
+var foo = require("testmodule");
+console.log("Bar property : " + foo.bar);
+foo.hello();
+*/
+
+
+/* -- Native Debugger ------------------ */
+
+//load("applications/NatBug.nap");
+
+/*
+
+var h = new Http("http://p.nf/post.php").request({
+    headers: {
+        "User-Agent": "firefox",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Connection": "close"
+    },
+    data: "gros=data&foo=bar",
+    timeout: 10000
+}, function(e) {
+    echo(e.data);
+});
+
+*/
