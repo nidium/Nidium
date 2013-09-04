de51e2723e1ebb3cadefa0ecf55d943b72fc30b3
dmx374 <dmx374@gmail.com>
2013-09-04 20:54:19 +0200

    UIStatus fixed
    Former-commit-id: d64591c62e7caa7c4bcbdfc6bec440bbcef514af [formerly ecffd89608e2dd51ae6340ba0c19f1c5b6ddbe4e]
    Former-commit-id: cd7721be2d0402dcab00b9737660ffab6ae2fd8e

--- framework/applications/components/http.js
+++ framework/applications/components/http.js
@@ -10,8 +10,9 @@ var main = new Application({
 });
 
 main.status = new UIStatus(main);
-main.status.open();
 main.status.progressBarColor = "rgba(210, 255, 60, 1)";
+main.status.progressBarLeft = 70;
+main.status.open();
 
 var url = "http://195.122.253.112/public/mp3/Symphony%20X/Symphony%20X%20'A%20Fool's%20Paradise'.mp3";
 //var url = "http://www.desktopwallpaperhd.net/wallpapers/4/7/landscape-beach-wallpaper-background-paradise-paradisebeach-high-41319.jpg";
@@ -26,7 +27,6 @@ var h = new HttpRequest('GET', url, null, function(e){
 });
 
 h.ondata = function(e){
-//	main.status.top = 748;
 	main.status.label = e.percent+"%";
 	main.status.value = e.percent;
 };
