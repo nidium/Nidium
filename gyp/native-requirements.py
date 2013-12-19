cf74367624b1173b1cd6960918512aed9ae993ec
efyx <n.trani@weelya.com>
2013-12-19 15:22:00 +0100

    Fix leveldb build on linux
    Former-commit-id: aab96b8c90967a580becf05aa0460330ee641d2d [formerly 2c5eb4f8eda54299ad60fef5264a763838d02e8e]
    Former-commit-id: 3707fe5494227901c8d0683bbb179fe01b857f16

--- gyp/native-requirements.py
+++ gyp/native-requirements.py
@@ -54,6 +54,13 @@ def buildLibCoroutine():
 
     deps.buildDep("libcoroutine", "libcoroutine", ["make"], outlibs=[["libcoroutine/_build/lib/liblibcoroutine", "libcoroutine"]])
 
+def buildLevelDB():
+    flags = ""
+    if deps.system == "Darwin":
+        flags = "CXXFLAGS='-stdlib=libc++ -mmacosx-version-min=10.7' CFLAGS='-mmacosx-version-min=10.7'"
+
+    deps.buildDep("libleveldb", "leveldb", [flags + " make"], outlibs=["leveldb/libleveldb"])
+
 def registerDeps():
     deps.registerDep("depot_tools",
         partial(deps.downloadDep, "depot_tools", deps.depsURL + "/depot_tools.tar.gz"),
@@ -94,7 +101,7 @@ def registerDeps():
 
     deps.registerDep("leveldb",
         partial(deps.downloadDep, "leveldb", deps.depsURL + "/leveldb.tar.gz"),
-        partial(deps.buildDep, "libleveldb", "leveldb", ["CXXFLAGS='-stdlib=libc++ -mmacosx-version-min=10.7' CFLAGS='-mmacosx-version-min=10.7' make"], outlibs=["leveldb/libleveldb"]))
+        buildLevelDB)
 
     deps.registerDep("skia", 
         downloadSkia,
