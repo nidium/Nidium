faadf4f35b78710a6cd367e600b126731e3c7584
paraboul <paraboul@gmail.com>
2014-01-22 20:04:08 +0100

    Add: NativeDB (leveldb binding)

---
+++ NativeDB.h
@@ -0,0 +1,49 @@
+#ifndef nativedb_h__
+#define nativedb_h__
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+
+namespace leveldb {
+    class DB;
+};
+
+namespace JS {
+    class Value;
+}
+
+class JSObject;
+struct JSContext;
+
+class NativeDB
+{
+    public:
+        explicit NativeDB(const char *name);
+        ~NativeDB();
+
+        /*
+            Check status after the constructor is caller
+        */
+        bool ok() const {
+            return m_Status;
+        }
+
+        bool insert(const char *key, const uint8_t *data, size_t data_len);
+        bool insert(const char *key, const char *string);
+        bool insert(const char *key, const std::string &string);
+
+        /*
+            Caller is responsible for knowing how to
+            decode the data during a get()
+        */
+        bool insert(const char *key, JSContext *cx, const JS::Value &val);
+
+        bool get(const char *key, std::string &ret);
+    private:
+        leveldb::DB *m_Database;
+        bool m_Status;
+};
+
+#endif
