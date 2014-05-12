2eeaf5ebedd2038a17a29e72b3675ead5b2f5eb9
Anthony Catel <paraboul@gmail.com>
2014-05-12 14:27:46 +0200

    Init Stream and Task manager

--- src/NativeContext.cpp
+++ src/NativeContext.cpp
@@ -3,11 +3,17 @@
 #include <NativeJS.h>
 #include "NativeJSConsole.h"
 #include <NativeMessages.h>
+#include <NativeStreamInterface.h>
+#include <NativeFileStream.h>
 
 NativeContext::NativeContext(ape_global *net)
 {
+
     m_JS = new NativeJS(net);
     m_JS->setPrivate(this);
+
+    NativePath::registerScheme(SCHEME_DEFINE("file://", NativeFileStream, false), true);
+    NativeTaskManager::createManager();
     NativeMessages::initReader(net);    
     m_JS->loadGlobalObjects();
 
