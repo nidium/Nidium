202f6eb86aa60265057daa20a1b4f8215b0b4e46
paraboul <paraboul@gmail.com>
2014-06-04 16:02:55 +0200

    Default clear value
    Former-commit-id: 7aaa266d00cd000a9dd9eab493e11d0f3fdbb333 [formerly 26891f75d19d8409cd0fc86c33194a6d6a30bdd7]
    Former-commit-id: 3552b867e34f8b2fceeeb3bdf4af5bcecdc8a198

--- src/NativeCanvasContext.h
+++ src/NativeCanvasContext.h
@@ -92,7 +92,7 @@ public:
     virtual void translate(double x, double y)=0;
     virtual void setSize(int width, int height, bool redraw = true)=0;
     virtual void setScale(double x, double y, double px=1, double py=1)=0;
-    virtual void clear(uint32_t color)=0;
+    virtual void clear(uint32_t color = 0x00000000)=0;
     virtual void flush()=0;
 
     /* Returns the size in device pixel */
