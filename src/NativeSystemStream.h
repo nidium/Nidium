5d305f292d1c8f08edd9edf7d5a7e16bd8edd4fe
paraboul <paraboul@gmail.com>
2014-09-14 11:37:46 +0200

    Adding NativeSystemStream.h
    Former-commit-id: 04084c5455eb01577014b11268ac9137a6b60d5f [formerly 2dad189ba79b7fa9389de5ee2b02f5a0f439c28a]
    Former-commit-id: e5d713d2752f0bbf950a01ea5d9a49c973032b6a

---
+++ src/NativeSystemStream.h
@@ -0,0 +1,78 @@
+/*
+    NativeJS Core Library
+    Copyright (C) 2014 Anthony Catel <paraboul@gmail.com>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef nativesystemstream_h__
+#define nativesystemstream_h__
+
+#include <NativeFileStream.h>
+#include <NativeSystemInterface.h>
+#include <string>
+
+
+class NativeSystemStream : public NativeFileStream
+{
+public:
+    explicit NativeSystemStream(const char *location) :
+        NativeFileStream(location)
+    {
+    }
+
+    static NativeBaseStream *createStream(const char *location) {
+        return new NativeSystemStream(location);
+    }
+
+    static bool allowLocalFileStream() {
+        return true;
+    }
+
+    static bool allowSyncStream() {
+        return true;
+    }
+
+    static const char *getBaseDir() {
+        return "/";
+    }
+};
+
+class NativeUserStream : public NativeFileStream
+{
+public:
+    explicit NativeUserStream(const char *location) :
+        NativeFileStream(location)
+    {
+    }
+
+    static NativeBaseStream *createStream(const char *location) {
+        return new NativeUserStream(location);
+    }
+
+    static bool allowLocalFileStream() {
+        return true;
+    }
+
+    static bool allowSyncStream() {
+        return true;
+    }
+
+    static const char *getBaseDir() {
+        return NativeSystemInterface::getInstance()->getUserDirectory();
+    }
+};
+
+#endif
