d53627ada2b26ec559f0b3fab0726654e28db080
efyx <n.trani@weelya.com>
2014-11-05 11:03:33 +0100

    NativeJSModules : Fix buffer overflow

--- NativeJSModules.cpp
+++ NativeJSModules.cpp
@@ -395,7 +395,7 @@ std::string NativeJSModules::findModuleInPath(NativeJSModule *module, const char
                     }
 
                     for (int j = 1; j < 4; j++) {
-                        size_t tmpPos = tmp.find(extensions[j], pos, tmp.length() - pos);
+                        size_t tmpPos = tmp.find(extensions[j], pos);
                         if (tmpPos != std::string::npos) {
                             switch (j) {
                                 case 1:
