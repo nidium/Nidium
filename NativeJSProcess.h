90efb21efd94c1c1144b98b94e844340a68d49bd
paraboul <paraboul@gmail.com>
2014-12-05 16:25:58 +0100

    Missing header

---
+++ NativeJSProcess.h
@@ -0,0 +1,22 @@
+#ifndef nativejsprocess_h__
+#define nativejsprocess_h__
+
+#include "NativeJSExposer.h"
+
+class NativeJSProcess : public NativeJSExposer<NativeJSProcess>
+{
+  public:
+    NativeJSProcess(JSObject *obj, JSContext *cx) :
+        NativeJSExposer<NativeJSProcess>(obj, cx) {};
+    virtual ~NativeJSProcess(){};
+
+    static void registerObject(JSContext *cx, char **argv, int argc);
+    static const char *getJSObjectName() {
+        return "process";
+    }
+
+    static JSClass *jsclass;
+};
+
+
+#endif
