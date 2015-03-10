2f2a0f435a095a2ec6c7d8760d3f7dd7dc78c4ff
Anthony Catel <paraboul@gmail.com>
2015-03-10 15:51:48 +0100

    Adding macro NATIVE_ENABLE_GL_ERROR, that can be enabled at compile that so that every gl calls are checked
    Former-commit-id: 8e8d0a4301b3301a91dc0f38f12925f978ca5fcf [formerly 67e47e262c74303bf904786d06799c7056ef99b4]
    Former-commit-id: a2065eb54fd91f36f7abe4974c859a7b42dfd3e5

--- src/NativeGLContext.h
+++ src/NativeGLContext.h
@@ -19,17 +19,41 @@ typedef void *SDL_GLContext;
 
 #define NATIVE_GL_MAIN_IFACE (__NativeUI->getNativeContext()->getGLState()->getNativeGLContext())
 
-#define NATIVE_GL_CALL(IFACE, X)                         \
-    do {                                                 \
-        NativeGLContext::GLCallback(IFACE->m_Interface);  \
-        (IFACE)->m_Interface->fFunctions.f##X;            \
-    } while (false)
+#ifndef NATIVE_ENABLE_GL_ERROR
 
-#define NATIVE_GL_CALL_RET(IFACE, X, RET)   \
-    do {                                    \
-        NativeGLContext::GLCallback(IFACE->m_Interface);  \
-        (RET) =  (IFACE)->m_Interface->fFunctions.f##X;   \
-    } while (false)
+    #define NATIVE_GL_CALL(IFACE, X)                         \
+        do {                                                 \
+            NativeGLContext::GLCallback(IFACE->m_Interface);  \
+            (IFACE)->m_Interface->fFunctions.f##X;            \
+        } while (false)
+
+    #define NATIVE_GL_CALL_RET(IFACE, X, RET)   \
+        do {                                    \
+            NativeGLContext::GLCallback(IFACE->m_Interface);  \
+            (RET) =  (IFACE)->m_Interface->fFunctions.f##X;   \
+        } while (false)
+
+#else
+    #define NATIVE_GL_CALL(IFACE, X)                         \
+        do {                                                 \
+            uint32_t __err;                                  \
+            NativeGLContext::GLCallback(IFACE->m_Interface); \
+            (IFACE)->m_Interface->fFunctions.f##X;           \
+            if ((__err = (IFACE)->m_Interface->fFunctions.fGetError()) != GR_GL_NO_ERROR) { \
+                NLOG("[Nidium GL Error : gl%s() returned %d", #X, __err);    \
+            } \
+        } while (false)
+
+    #define NATIVE_GL_CALL_RET(IFACE, X, RET)   \
+        do {                                    \
+            uint32_t __err; \
+            NativeGLContext::GLCallback(IFACE->m_Interface);  \
+            (RET) =  (IFACE)->m_Interface->fFunctions.f##X;   \
+            if ((__err = (IFACE)->m_Interface->fFunctions.fGetError()) != GR_GL_NO_ERROR) { \
+                NLOG("[Nidium GL Error : gl%s() returned %d", #X, __err);    \
+            } \
+        } while (false)
+#endif
 
 #define NATIVE_GL_CALL_MAIN(X) NATIVE_GL_CALL((NATIVE_GL_MAIN_IFACE), X) 
 
