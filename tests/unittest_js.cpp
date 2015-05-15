e76b7bfce505d231ae5f8b4dede31e6aff85d431
Peter Reijnders <Peter.Reijnders@verpeteren.nl>
2015-05-15 13:15:27 +0200

    Added more tests for NativeJS.

--- tests/unittest_js.cpp
+++ tests/unittest_js.cpp
@@ -6,21 +6,27 @@
 #include <native_netlib.h>
 #include <NativeJS.h>
 
-static int counter;
+static int logcounter;
+static int msgcounter;
+void msg_cb_t(JSContext *cx, NativeSharedMessages::Message *msg)
+{
+	msgcounter++;
+	ape_running = 0;
+}
 
 int dummyLogger(const char *format)
 {
-	return counter += strcmp(format, "tt %s") ;
+	return logcounter += strcmp(format, "tt %s") ;
 }
 
 int dummyVLogger(const char *format, va_list ap)
 {
-	return counter += strcmp(format, "tt %s");
+	return logcounter += strcmp(format, "tt %s");
 }
 
 int dummyLoggerClear()
 {
-	counter = -10;
+	logcounter = -10;
 	return 0;
 }	
 
@@ -66,36 +72,36 @@ TEST(NativeJS, Simple)
 	//store objects
 	d = JS_NewObject(njs.cx, NULL, NULL, NULL);
 	rval = INT_TO_JSVAL(1);
-	JS_SetProperty(njs.cx, d, "nidium", &rval);
+	JS_SetProperty(njs.cx, d, "a", &rval);
 	njs.rootObjectUntilShutdown(d);
 	njs.unrootObject(d);
 	dc =JS_NewObject(njs.cx, NULL, NULL, NULL);
 	njs.copyProperties(njs.cx, d, dc);
-	JS_GetProperty(njs.cx, dc, "nidium", &rval);
+	JS_GetProperty(njs.cx, dc, "a", &rval);
 	EXPECT_EQ(JSVAL_TO_INT(rval), 1);
 
 	//check the loggers
-	counter = 0;
+	logcounter = 0;
 	njs.log("%s");
-	EXPECT_EQ(counter, 0);
+	EXPECT_EQ(logcounter, 0);
 
 	njs.setLogger(dummyLogger);
-	njs.logf("tt %s", "nidium");
-	EXPECT_EQ(counter, 0);
+	njs.logf("tt %s", "a");
+	EXPECT_EQ(logcounter, 0);
 	njs.log("%s");
-	EXPECT_EQ(counter, -1);
+	EXPECT_EQ(logcounter, -1);
 
 	njs.setLogger(dummyVLogger);
-	njs.logf("tt %s", "nidium");
-	EXPECT_EQ(counter, -1);
+	njs.logf("tt %s", "a");
+	EXPECT_EQ(logcounter, -1);
 	njs.log("%s");
-	EXPECT_EQ(counter, -2);
+	EXPECT_EQ(logcounter, -2);
 	
 	njs.logclear();
-	EXPECT_EQ(counter, -2);
+	EXPECT_EQ(logcounter, -2);
 	njs.setLogger(dummyLoggerClear);
 	njs.logclear();
-	EXPECT_EQ(counter, -10);
+	EXPECT_EQ(logcounter, -10);
 
 	ape_running = 0;
 
@@ -113,20 +119,88 @@ TEST(NativeJS, Quick)
 	EXPECT_TRUE(njs.net == g_ape);
 	EXPECT_TRUE(njs.getNet() == g_ape);
 	
+	//native_netlib_destroy(g_ape);
+}
+
+TEST(NativeJS, Code)
+{	
+	ape_global *g_ape = native_netlib_init();
+	NativeJS njs(g_ape);
+	const char * srcA = "var a = 11*11;";
+	const char * srcB = "b = a - 21";
+	jsval rval;
+	int success;
+
+	success = njs.LoadScriptContent(srcA, strlen(srcA), __FILE__);
+	EXPECT_EQ(success, 1);
+	JS_GetProperty(njs.cx, JS_GetGlobalObject(njs.cx), "a", &rval);
+	EXPECT_EQ(JSVAL_TO_INT(rval), 121);
+
+	success = njs.LoadScriptReturn(njs.cx, srcB, strlen(srcB), __FILE__, &rval);
+	EXPECT_EQ(success, 1);
+	EXPECT_EQ(JSVAL_TO_INT(rval), 100);
+	JS_GetProperty(njs.cx, JS_GetGlobalObject(njs.cx), "b", &rval);
+	EXPECT_EQ(JSVAL_TO_INT(rval), 100);
+
+#if 0
+	const char * srcC = "var c = b * a";
+	const char * srcD = "var d = c - a";
+	NativeBytecodeScript bc;
+
+	success = njs.LoadBytecode((void*)srcC, strlen(srcC), __FILE__);
+	EXPECT_EQ(success, 0);  // bad script XDR, but comilies
+	JS_GetProperty(njs.cx, JS_GetGlobalObject(njs.cx), "c", &rval);
+	EXPECT_EQ(JSVAL_TO_INT(rval), 12100);
+
+	bc.name = "dummy";
+	bc.size = strlen(srcD);
+	bc.data = (const unsigned char*) srcD;
+	success = njs.LoadBytecode(&bc);
+	EXPECT_EQ(success, 1);
+	JS_GetProperty(njs.cx, JS_GetGlobalObject(njs.cx), "d", &rval);
+	EXPECT_EQ(JSVAL_TO_INT(rval), 12079);
+	
+#endif
+//@TODO: static int LoadScriptReturn(JSContext *cx, const char *filename, JS::Value *ret);
+//@TODO: int LoadScript(const char *filename);
+
+	//native_netlib_destroy(g_ape);
+}
+TEST(NativeJS, Messages)
+{
+	ape_global *g_ape = native_netlib_init();
+	NativeJS njs(g_ape);
+	size_t i;
+	
+	EXPECT_EQ(njs.registeredMessagesIdx, 8);
+	EXPECT_EQ(njs.registeredMessagesSize, 16);
+	for( i = 0; i < 8; i++) {	
+		EXPECT_TRUE(njs.registeredMessages[i] == NULL);
+	}
+	njs.registerMessage( msg_cb_t);
+	EXPECT_EQ(njs.registeredMessagesIdx, 9);
+	EXPECT_EQ(njs.registeredMessagesSize, 16);
+	for( i = 10; i < 18; i++) {	
+		printf("%d %d %d\n", i, njs.registeredMessagesIdx, njs.registeredMessagesSize);
+		njs.registerMessage( msg_cb_t);
+		EXPECT_TRUE(njs.registeredMessages[i] != NULL);
+	}
+	EXPECT_EQ(njs.registeredMessagesIdx, 17);
+	EXPECT_EQ(njs.registeredMessagesSize, 32);
+
+	njs.registerMessage( msg_cb_t, 0);
+	//@FIXME: njs.registerMessage( msg_cb_t, 8);
+	//@FIXME: njs.registerMessage(msg_cb_t, 222);	
+#if 0
+	msgcounter = 0;
+	ape_running = 1;
+	void postMessage(void *dataPtr, int ev);
+	events_loop(g_ape);
+	EXPECT_EQ(msgcounter, 1);
+#endif
+	//native_netlib_destroy(g_ape);
 }
 /*
-static int LoadScriptReturn(JSContext *cx, const char *data, size_t len, const char *filename, JS::Value *ret);
-static int LoadScriptReturn(JSContext *cx, const char *filename, JS::Value *ret);
-
-int LoadScriptContent(const char *data, size_t len, const char *filename);
-int LoadScript(const char *filename);
-int LoadBytecode(NativeBytecodeScript *script);
-int LoadBytecode(void *data, int size, const char *filename);
-
-int registerMessage(native_thread_message_t cbk);
-void registerMessage(native_thread_message_t cbk, int id);
-void postMessage(void *dataPtr, int ev);
-
 static JSStructuredCloneCallbacks *jsscc;
 static JSObject *readStructuredCloneOp(JSContext *cx, JSStructuredCloneReader *r, uint32_t tag, uint32_t data, void *closure);
 static JSBool writeStructuredCloneOp(JSContext *cx, JSStructuredCloneWriter *w, JSObject *obj, void *closure);
