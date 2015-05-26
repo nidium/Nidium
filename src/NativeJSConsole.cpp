5e6ae717d705c0b84751012a5255e66277676d00
Peter Reijnders <Peter.Reijnders@verpeteren.nl>
2015-05-26 13:36:21 +0200

    Corrected JS_DefineObject.

--- src/NativeJSConsole.cpp
+++ src/NativeJSConsole.cpp
@@ -103,9 +103,8 @@ static JSBool native_console_log(JSContext *cx, unsigned argc,
 
 void NativeJSconsole::registerObject(JSContext *cx)
 {
-    JSObject *consoleObj;
-    consoleObj = JS_DefineObject(cx, JS_GetGlobalObject(cx), "console",
-        &console_class , NULL, 0);
+    JS::RootedObject consoleObj(cx, JS_DefineObject(cx, JS_GetGlobalObject(cx),
+        "console", &console_class , NULL, 0));
     JS_DefineFunctions(cx, consoleObj, console_funcs);
 }
 
