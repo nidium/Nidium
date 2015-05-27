19510f63dc639671bafd9e9f31e9d2bcc1e06e65
paraboul <paraboul@gmail.com>
2015-05-27 11:49:47 +0200

    Add script that change the mozilla-central symlink (processed by a git hook)

---
+++ swap-mozversion.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -f /Users/paraboul/dev/NativeStudio/third-party/mozilla-central
+ln -s /Users/paraboul/dev/NativeStudio/third-party/mozjs-24.0.0 /Users/paraboul/dev/NativeStudio/third-party/mozilla-central
+
