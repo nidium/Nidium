35f274d8f4016719acb584d492c3326508547d68
Peter Reijnders <peter.reijnders@verpeteren.nl>
2016-02-10 13:08:20 +0100

    fixed typo's and inconsistancies in the rawdocs

--- var/rawdoc/NativeJSConsole.cpp.py
+++ var/rawdoc/NativeJSConsole.cpp.py
@@ -1,58 +1,58 @@
 from dokumentor import *
 
-NamespaceDoc( "Console", "Output, profile and logging functions",
+NamespaceDoc( "Console", "Output, profile and logging functions.",
 	NO_Sees,
 	[ ExampleDoc( "console.log( 'Nidium' );" ) ]
-	)
+)
 
-if False:
-	FunctionDoc( "Console.profile", "Starts a profiler instance",
-		[ SeeDoc( "Console.profileEnd" ) ],
-		ExampleDoc( """console.profile( );
-		// Time intense task
-		var result = Console.profileEnd( );
-		console.log( JSON.stringify( result ) ); """),
-		IS_Static, IS_Public, IS_Fast,
-		NO_Params,
-		NO_Returns
-	)
-
-	FunctionDoc( "Console.profileEnd", "Stops a profiler instance",
-		[ SeeDoc( "Console.profile" ) ],
-		ExampleDoc( """console.profile( );
-		// Time intense task
-		var result = Console.profileEnd( );
-		console.log( JSON.stringify( result ) ); """),
-		IS_Static, IS_Public, IS_Fast,
-		NO_Params,
-		ReturnDoc( "Profile results", "Object" )
-	)
-
-FunctionDoc( "Console.log", "Logs output to the console", 
-	[ SeeDoc( "Console.info" ), SeeDoc( "Console.info" ), SeeDoc( "Console.warn" ), SeeDoc( "Console.write" ), SeeDoc( "Console.clear" ) ],
-	ExampleDoc( "console.log( 'Nidium' );" ), 
+FunctionDoc( "Console.profile", "Starts a profiler instance.",
+	[ SeeDoc( "Console.profileEnd|global.console" ) ],
+	ExampleDoc( """console.profile( );
+	// Time intense task
+	var result = Console.profileEnd( );
+	console.log( JSON.stringify( result ) ); """),
 	IS_Static, IS_Public, IS_Fast,
-	[ParamDoc( "text", "The stringlist that should be logged", "string", NO_Default, IS_Optional )], 
+	NO_Params,
 	NO_Returns
 )
-FunctionDoc( "Console.info", "Logs output to the console, marked as info",
+
+FunctionDoc( "Console.profileEnd", "Stops a profiler instance.",
+	[ SeeDoc( "Console.profile|global.console" ) ],
+	ExampleDoc( """console.profile( );
+	// Time intense task
+	var result = Console.profileEnd( );
+	console.log( JSON.stringify( result ) ); """),
+	IS_Static, IS_Public, IS_Fast,
+	NO_Params,
+	ReturnDoc( "Profile results", "Object" )
+)
+
+FunctionDoc( "Console.log", "Logs output to the console.",
+	[ SeeDoc( "Console.info" ), SeeDoc( "Console.info" ), SeeDoc( "Console.warn" ), SeeDoc( "Console.write" ), SeeDoc( "Console.clear" ) ],
+	ExampleDoc( "console.log( 'Nidium' );" ),
+	IS_Static, IS_Public, IS_Fast,
+	[ParamDoc( "text", "The string that should be logged", "string", NO_Default, IS_Optional )],
+	NO_Returns
+)
+FunctionDoc( "Console.info", "Logs output to the console, marked as info.",
 	[ SeeDoc( "Console.log" ), SeeDoc( "Console.info" ), SeeDoc( "Console.warn" ), SeeDoc( "Console.write" ), SeeDoc( "Console.clear" ) ],
 	ExampleDoc( "console.info( 'Nidium, A new breed of browser' );" ),
 	IS_Static, IS_Public, IS_Fast,
-	[ParamDoc( "text", "The stringlist that should be logged", "string", NO_Default, IS_Optional )],
+	[ParamDoc( "text", "The string that should be logged", "string", NO_Default, IS_Optional )],
 	NO_Returns
 )
-FunctionDoc( "Console.error", "Logs output to the console, marked as error",
+FunctionDoc( "Console.error", "Logs output to the console, marked as error.",
 	[ SeeDoc( "Console.log" ), SeeDoc( "Console.info" ), SeeDoc( "Console.warn" ), SeeDoc( "Console.write" ), SeeDoc( "Console.clear" ) ],
 	ExampleDoc( "console.error( 'Nidium, Cannot display HTML' );" ),
 	IS_Static, IS_Public, IS_Fast,
-	[ParamDoc( "text", "The stringlist that should be logged", "string", NO_Default, IS_Optional )],
+	[ParamDoc( "text", "The string that should be logged", "string", NO_Default, IS_Optional )],
 	NO_Returns
 )
-FunctionDoc( "Console.warn", "Logs output to the console, marked as warn",
+FunctionDoc( "Console.warn", "Logs output to the console, marked as warn.",
 	[ SeeDoc( "Console.log" ), SeeDoc( "Console.info" ), SeeDoc( "Console.info" ), SeeDoc( "Console.write" ), SeeDoc( "Console.clear" ) ],
 	ExampleDoc( "console.warn( 'Nidium, Improving the web' );" ),
 	IS_Static, IS_Public, IS_Fast,
-	[ParamDoc( "text", "The stringlist that should be logged", "string", NO_Default, IS_Optional )],
+	[ParamDoc( "text", "The string that should be logged", "string", NO_Default, IS_Optional )],
 	NO_Returns
 )
+
--- var/rawdoc/NativeJSSystem.cpp.py
+++ var/rawdoc/NativeJSSystem.cpp.py
@@ -1,14 +1,15 @@
 from dokumentor import *
 
-NamespaceDoc( "System", "System statistics",
+NamespaceDoc( "System", "System statistics.",
 	NO_Sees,
 	NO_Examples
 )
 
-FunctionDoc( "System.getOpenFileStats", "Provide information about the openfiles",
+FunctionDoc( "System.getOpenFileStats", "Provide information about the openfiles.",
 	NO_Sees,
 	[ExampleDoc( "console.log( JSON.stringify( System.getOpenFileStats() ));" )],
 	IS_Static, IS_Public, IS_Fast,
 	NO_Params,
 	ReturnDoc( "Object with keys: cur/integer, max/integer, open/integer, sockets/integer, files/integer", "Object")
 )
+
