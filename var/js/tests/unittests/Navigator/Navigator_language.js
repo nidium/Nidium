ce66df7e3d981beca58178946a8cc7b2f8ce6b07
efyx <n.trani@weelya.com>
2016-03-02 00:26:59 +0100

    Tests/Navigator : Fix language test
    Former-commit-id: 1fa8111ae59e0a1dd00dbaa9cdc51b948c01c45c [formerly dbdd788e373cd98abc95646b1af36d75511769b9]
    Former-commit-id: 0624f06b64cbb4350e6a2f77ce5309412e4d8cc5

--- var/js/tests/unittests/Navigator/Navigator_language.js
+++ var/js/tests/unittests/Navigator/Navigator_language.js
@@ -1,5 +1,5 @@
 // XXX : Obviously this script will fail to run on non en-us computer...
 Tests.register("Navigator.language", function() {
-    Assert.equal(navigator.language, 'en-us');
+    Assert.equal(navigator.language, 'en');
 });
 
