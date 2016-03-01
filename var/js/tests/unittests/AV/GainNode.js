867f2193c99cf66f9fdba2daf4502d0550594b0d
efyx <n.trani@weelya.com>
2016-03-01 19:02:11 +0100

    Tests/AudioNodeGain : Add tests
    Former-commit-id: 34883ade27e0ce221afb274a8b0baaedee15ce00 [formerly c64f71c0815adfea62d3372a5682ce81abcf238b]
    Former-commit-id: a7b16b43b8eed10f516d3bf275499985e0e1561d

---
+++ var/js/tests/unittests/AV/GainNode.js
@@ -0,0 +1,58 @@
+var dsp;
+var source;
+var customProcessor;
+var gain;
+
+Tests.register("CustomSourceNode.create", function() {
+    dsp = Audio.getContext();
+    source = dsp.createNode("custom-source", 0, 2);
+    gain = dsp.createNode("gain", 2, 2);
+    customProcessor = dsp.createNode("custom", 2, 2);
+    var target = dsp.createNode("target", 2, 0);
+  
+    dsp.connect(source.output[0], gain.input[0]);
+    dsp.connect(source.output[1], gain.input[1]);
+
+    dsp.connect(gain.output[0], customProcessor.input[0]);
+    dsp.connect(gain.output[1], customProcessor.input[1]);
+
+    dsp.connect(customProcessor.output[0], target.input[0]);
+    dsp.connect(customProcessor.output[1], target.input[1]);
+
+    Assert(source instanceof AudioNode);
+});
+
+Tests.registerAsync("GainNode.set & get", function(next) {
+    gain.set("gain", 0.5);
+
+    customProcessor.onmessage = function(msg) {
+        customProcessor.onmessage = null;
+        customProcessor.assignProcessor(null);
+        source.assignProcessor(null);
+
+        Assert.equal(msg.data, true);
+
+        source.stop();
+
+        next();
+    }
+
+    customProcessor.assignProcessor(function(frames, scope) {
+        var ok = false;
+        for (var i = 0; i < frames.data.length; i++) {
+            if (frames.data[i][frames.size - 1] == 21) {
+                ok = true;
+            }
+        } 
+        this.send(ok);
+    });
+
+    source.assignProcessor(function(frames, scope) {
+        for (var i = 0; i < frames.data.length; i++) {
+            frames.data[i][frames.size - 1] = 42;
+        } 
+    });
+
+    source.play();
+
+}, 5000);
