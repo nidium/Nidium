81b27d3e258c7d988e2c7d6d0c25d699e234204f
Peter Reijnders <peter.reijnders@verpeteren.nl>
2016-04-09 06:25:48 +0200

    Added MIT-License reference to tests

--- tests/unittest_streaminterface.cpp
+++ tests/unittest_streaminterface.cpp
@@ -1,3 +1,8 @@
+/*
+   Copyright 2016 Nidium Inc. All rights reserved.
+   Use of this source code is governed by a MIT license
+   that can be found in the LICENSE file.
+*/
 #include <stdlib.h>
 #include <string.h>
 
