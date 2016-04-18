94dbb393ebd6545e587fff9361b6aa10cb96d20d
Peter Reijnders <peter.reijnders@verpeteren.nl>
2016-04-18 07:59:54 +0200

    Prepared changes for Path namespace

--- src/NativeContext.cpp
+++ src/NativeContext.cpp
@@ -5,7 +5,7 @@
 #include <native_netlib.h>
 
 #include <Core/Messages.h>
-#include <Core/NativePath.h>
+#include <Core/Path.h>
 #include <IO/FileStream.h>
 #include <Net/HTTPStream.h>
 #include <Binding/NidiumJS.h>
@@ -38,8 +38,8 @@ NativeContext::NativeContext(ape_global *net, NativeWorker *worker,
     if (getcwd(cwd, sizeof(cwd)-1) != NULL) {
         strcat(cwd, "/");
 
-        NativePath::cd(cwd);
-        NativePath::chroot("/");
+        Nidium::Core::Path::cd(cwd);
+        Nidium::Core::Path::chroot("/");
     } else {
         fprintf(stderr, "[Warn] Failed to get current working directory\n");
     }
@@ -48,9 +48,9 @@ NativeContext::NativeContext(ape_global *net, NativeWorker *worker,
     m_JS->setPrivate(this);
     m_JS->setStrictMode(jsstrict);
 
-    NativePath::registerScheme(SCHEME_DEFINE("file://", Nidium::IO::FileStream, false), true);
-    NativePath::registerScheme(SCHEME_DEFINE("http://", Nidium::Net::HTTPStream,    true));
-    NativePath::registerScheme(SCHEME_DEFINE("https://", Nidium::Net::HTTPStream,    true));
+    Nidium::Core::Path::registerScheme(SCHEME_DEFINE("file://", Nidium::IO::FileStream, false), true);
+    Nidium::Core::Path::registerScheme(SCHEME_DEFINE("http://", Nidium::Net::HTTPStream,    true));
+    Nidium::Core::Path::registerScheme(SCHEME_DEFINE("https://", Nidium::Net::HTTPStream,    true));
 
     Nidium::Core::TaskManager::createManager();
     Nidium::Core::Messages::initReader(net);
