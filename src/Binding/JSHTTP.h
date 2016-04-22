e487500f4c5d72b79650e688990803bf9c3c3ebd
efyx <n.trani@weelya.com>
2016-04-22 14:01:48 +0200

    JSHTTP : Rename nativejshttp_h define to binding_jshttp_h

--- src/Binding/JSHTTP.h
+++ src/Binding/JSHTTP.h
@@ -3,8 +3,8 @@
    Use of this source code is governed by a MIT license
    that can be found in the LICENSE file.
 */
-#ifndef nativejshttp_h__
-#define nativejshttp_h__
+#ifndef binding_jshttp_h__
+#define binding_jshttp_h__
 
 #include <ape_netlib.h>
 #include <ape_array.h>
