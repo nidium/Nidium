9e260e184e0092f9bebfaf07bc23fecdcc0b8385
efyx <n.trani@weelya.com>
2016-06-10 00:17:37 +0200

    Preload : Add callback function

--- src/Embed/preload.js
+++ src/Embed/preload.js
@@ -19,3 +19,6 @@
 /* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        */
 /* DEALINGS IN THE SOFTWARE.                                                  */
 /* -------------------------------------------------------------------------- */
+
+function __nidiumPreload(options) {
+}
