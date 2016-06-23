1434f82fde7138fc9d5f6ce6b9ccc0780f35c8ad
efyx <n.trani@weelya.com>
2016-06-23 02:30:26 +0200

    Tests/JSFile : Update File.isDir() tests

--- tests/jsunittest/File/File_isDir.js
+++ tests/jsunittest/File/File_isDir.js
@@ -5,25 +5,33 @@
 */
 
 Tests.register("File.isDir",  function() {
-	var cases = {
-	'.': true, 
-	'./': true, 
-	'..': true, 
-	'../': true, 
-	'../File': true, 
-	'../File/': true, 
-	'doesexists': true,
-	'./doesexists': true,
-	'./doesnotexist': false, 
-	'doesnotexist': false, 
-	'..': false		//notallowed
-	};
-for( var path  in cases) {
-	var expected = cases[path];
-	var file_object = new File(path);
-	//console.log(JSON.stringify(file_object));
-	Assert.equal(file_object.isDir(), expected);
-}
+    var cases = {
+        '.': true, 
+        './': true, 
+        './File': true, 
+        './File/': true, 
+    };
+
+    for(var path in cases) {
+        var expected = cases[path];
+        var fileObject = new File(path);
+
+        fileObject.openSync("r");
+
+        Assert.equal(fileObject.isDir(), expected, "Path : \"" + path + "\" should be a " + (cases[path] ? "directory" : "file"));
+    }
 
 });
 
+Tests.register("File inexistent path", function() {
+    Assert.throws(function() {
+        var f = new File("does_not_exists");
+        f.openSync("r");
+    });
+});
+
+Tests.register("File outside root", function() {
+    Assert.throws(function() {
+        new File("..");
+    });
+});
