c275417043d30bce110568290e80976588ea5c84
Anthony Catel <paraboul@gmail.com>
2016-10-13 18:26:33 +0200

    Test: add basic Socket tests

---
+++ tests/jsunittest/Socket/basic.js
@@ -0,0 +1,39 @@
+Tests.registerAsync("Socket connect", function(next) {
+    var client = new Socket("127.0.0.1", 8000).connect();
+
+    client.onconnect = function() {
+        Assert.equal(client, this);
+        client.disconnect();
+        next();
+    }
+}, 500);
+
+Tests.registerAsync("Socket listen", function(next) {
+    var server = new Socket("127.0.0.1", 9027).listen();
+
+    var client = new Socket("127.0.0.1", 9027).connect();
+
+    server.onaccept = function(new_client) {
+        new_client.foo = "bar";
+        new_client.write("data");
+
+        Assert.equal(new_client.ip, "127.0.0.1");
+    }
+
+    client.onread = function(data) {
+        Assert.equal(data, "data");
+        this.write("reply");
+    }
+
+    server.onread = function(new_client, data) {
+        Assert.equal(data, "reply");
+        Assert.equal(new_client.foo, "bar");
+
+        new_client.disconnect();
+    }
+
+    client.ondisconnect = function() {
+        next();
+    }
+
+}, 500);
