fa087e67a145ab0be66b5cd6b0356a1067c44e88
Anthony Catel <paraboul@gmail.com>
2016-11-01 20:46:51 +0100

    JSAPI45: jschar -> uint16_t

--- src/Binding/JSModules.cpp
+++ src/Binding/JSModules.cpp
@@ -375,15 +375,15 @@ JS::Value JSModule::require(char *name)
                 }
             } else {
                 size_t len;
-                jschar *jchars;
+                uint16_t *jchars;
                 JS::RootedValue jsonData(m_Cx);
 
                 if (!JS_DecodeBytes(m_Cx, data, filesize, NULL, &len)) {
                     return ret;
                 }
 
-                jchars = static_cast<jschar *>(
-                    JS_malloc(m_Cx, len * sizeof(jschar)));
+                jchars = static_cast<uint16_t *>(
+                    JS_malloc(m_Cx, len * sizeof(uint16_t)));
                 if (!jchars) {
                     return ret;
                 }
