85954719b1512d5114b18daff400be5ba9be7590
Anthony Catel <paraboul@gmail.com>
2016-11-18 19:48:26 +0100

    AnimationBlock: optimize animation loop

--- src/Embed/AnimationBlock.js
+++ src/Embed/AnimationBlock.js
@@ -1,3 +1,26 @@
+/* -------------------------------------------------------------------------- */
+/* MIT license                                          (c) 2016 Nidium, Inc. */
+/* -------------------------------------------------------------------------- */
+/* Permission is hereby granted, free of charge, to any person obtaining a    */
+/* copy of this software and associated documentation files (the "Software"), */
+/* to deal in the Software without restriction, including without limitation  */
+/* the rights to use, copy, modify, merge, publish, distribute, sublicense,   */
+/* and/or sell copies of the Software, and to permit persons to whom the      */
+/* Software is furnished to do so, subject to the following conditions:       */
+/*                                                                            */
+/* The above copyright notice and this permission notice shall be included in */
+/* all copies or substantial portions of the Software.                        */
+/*                                                                            */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    */
+/* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    */
+/* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        */
+/* DEALINGS IN THE SOFTWARE.                                                  */
+/* -------------------------------------------------------------------------- */
+
+
 var Easing = require("easing");
 
 var AnimationsList = new Set();
@@ -6,17 +29,19 @@ var AnimationsList = new Set();
     let draw = function() {
         let curDate = +new Date();
 
-        for (let elem of AnimationsList) {
-            let { end, start, target, property, value, startValue, ease } = elem;
+        for (let anim of AnimationsList) {
+            let { end, start, ease, list } = anim;
+            let e = ease((curDate - start) / (end - start));
+            let finish = (curDate > end);
 
-            if (curDate > end) {
-                target[property] = value;
-                AnimationsList.delete(elem);
 
-            } else {
-                let e = ease((curDate - start) / (end - start));
+            for (let elem of list) {
+                let { target, property, value, startValue } = elem;
+                target[property] = finish ? value : startValue + ((value - startValue) * e);                
+            }
 
-                target[property] = startValue + ((value - startValue) * e);
+            if (finish) {
+                AnimationsList.delete(anim);
             }
         }
 
@@ -29,24 +54,30 @@ var AnimationsList = new Set();
 var AnimationBlock = function(duration, ease, callback, ...objs)
 {
     var proxies = [];
+    let start = +new Date();
+
+    var anim = {
+        end: start+duration,
+        ease,
+        start,
+        list: []
+    }
+
+    AnimationsList.add(anim);
+
     for (let obj of objs) {
         let proxy = new Proxy(obj, {
-            set: (target, property, value, receiver) => {
+            set: (target, property, value, rcv) => {
                 if (!(property in target)) {
                     return true;
                 }
 
-                let start = +new Date();
-
-                AnimationsList.add({
-                    end: start + duration,
+                anim.list.push({
                     startValue: target[property],
-                    start,
                     target,
                     property,
-                    value,
-                    ease
-                });
+                    value,                 
+                })
                 
                 return true;
             },
@@ -59,5 +90,9 @@ var AnimationBlock = function(duration, ease, callback, ...objs)
         proxies.push(proxy);
     }
 
-    callback(...proxies);
+    let next = callback(...proxies);
+    if (typeof next == 'function') {
+        //AnimationBlock(duration, ease, next, )
+    }
+
 }
