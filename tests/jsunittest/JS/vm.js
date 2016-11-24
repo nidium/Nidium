5cc7725e6a92c9b5a454a6118aaebc8e4d483339
Anthony Catel <paraboul@gmail.com>
2016-11-25 00:18:30 +0100

    Remove debug

--- tests/jsunittest/JS/vm.js
+++ tests/jsunittest/JS/vm.js
@@ -4,10 +4,6 @@ Tests.register("vm.runInScope", function() {
     var obj = {foo: "bar"};
 
     vm.runInScope(`
-        if (1 == 1) {
-            console.log("1 is 1");
-        }
-
         if (foo == "bar") {
             var newprop = 42;
         }
