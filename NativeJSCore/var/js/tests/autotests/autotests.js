73dfb70448ae678a2be23a5702e06c97b10d73f7
John Fischer <jfroffice@gmail.com>
2016-12-07 22:05:56 +0100

    Inject /NativeServer /NativeStudio /NativeJSCore history

--- NativeJSCore/var/js/tests/autotests/autotests.js
+++
@@ -1,8 +0,0 @@
-var TestsRunner = require('../../../../../nativejscore/var/js/tests/testrunner.js');
-var Suites = ['auto_suites.js'];
-
-var tr = new TestsRunner();
-tr.load(Suites);
-tr.run(function() {
-	tr.report(exit=true);
-});
