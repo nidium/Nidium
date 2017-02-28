b5492036262d367f18f213fa4965a08f5b6e1b92
efyx <n.trani@weelya.com>
2017-02-28 17:09:02 +0100

    Missing file in previous commit

--- src/Embed/lib/Elements/index.js
+++ src/Embed/lib/Elements/index.js
@@ -6,10 +6,9 @@
 
 const Elements = module.exports = {};
 
-const VM            = require("VM");
-const ElementStyles = require("./ElementStyles.js");
-const ShadowRoot    = require("../ShadowRoot.js");
-const s_ShadowRoot  = require("../Symbols.js").ElementShadowRoot;
+const ElementStyles = require("./Styles.js");
+const ShadowRoot    = require("../../ShadowRoot.js");
+const s_ShadowRoot  = require("../../Symbols.js").ElementShadowRoot;
 const s_NodeName    = Symbol("NodeName");
 const s_NodeID      = Symbol("NodeID");
 
@@ -529,74 +528,9 @@ Elements.img = class extends Elements.Element {
 
 }
 
-/*
-    <layout> implementation
-*/
-Elements.layout = class extends Elements.Node { }
-
-/*
-    <script> implementation
-*/
-Elements.script = class extends Elements.Node {
-    constructor(attributes) {
-        super(attributes);
-        this.hide();
-        if (attributes.src) {
-            this.data = Elements.Loader(attributes);
-        }
-    }
-
-    createTree(children) {
-        let code = this.data;
-        if (!code) {
-            code = children[0].text;
-        }
-        VM.run(code, {"scope": this[s_ShadowRoot].getJSScope()})
-    }
-}
-
-/*
-    <nss> implementation
-*/
-class NSS {
-    constructor(shadowRoot, nss, filename) {
-        this.shadowRoot = shadowRoot;
-        this.filename   = filename;
-        this.nss        = nss;
-
-        this.shadowRoot.addNSS(this);
-    }
-
-    eval() {
-        return VM.run(`(function() { return {${this.nss}} })()`, {
-            "scope": this.shadowRoot.getJSScope(),
-            "filename": this.filename
-        })
-    }
-}
-
-Elements.nss = class extends Elements.Node {
-    constructor(attributes) {
-        super(attributes);
-
-        this.hide();
-
-        this.filename = "inline nss";
-
-        if (attributes.src) {
-            this.data = Elements.Loader(attributes);
-            this.filename = attributes.src;
-        }
-    }
-
-    createTree(children) {
-        let nss = this.data;
-        if (!nss) {
-            nss = children[0].text;
-        }
-        this.nss = new NSS(this[s_ShadowRoot], nss, this.filename);
-    }
-}
+load("embed://lib/Elements/layout.js");
+load("embed://lib/Elements/nss.js");
+load("embed://lib/Elements/script.js");
 
 window._onready = function(lst) {}
 
