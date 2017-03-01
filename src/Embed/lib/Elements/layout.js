f987bbdc5d2c25aca338e1499c8bebc6a4986cc7
efyx <n.trani@weelya.com>
2017-03-01 12:35:05 +0100

    Elements : Make <layout> take the full width of it's parent
    This change is needed in order to allow correct sizing of <layout>
    children

--- src/Embed/lib/Elements/layout.js
+++ src/Embed/lib/Elements/layout.js
@@ -1,5 +1,14 @@
 {
     const Elements = require("Elements");
 
-    Elements.layout = class extends Elements.Node { }
+    Elements.layout = class extends Elements.Node {
+        constructor(attr) {
+            super(attr)
+            this.addEventListener("load", () => {
+                // Expand layout to take full width of it's parent
+                // to allow correct sizing of it's children
+                this.width = this.getParent().width;
+            });
+        }
+    }
 }
