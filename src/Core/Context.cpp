888979445a71bdf9afdbe7cbec13517f44630ea0
efyx <n.trani@weelya.com>
2017-03-03 13:47:20 +0100

    Context : Fix previous commit that breaks server build
    Interface class is only available for Frontend

--- src/Core/Context.cpp
+++ src/Core/Context.cpp
@@ -9,7 +9,9 @@
 #include "Core/Messages.h"
 #include "Net/HTTPStream.h"
 #include "IO/FileStream.h"
+#ifdef NIDIUM_PRODUCT_FRONTEND
 #include "Interface/SystemInterface.h"
+#endif
 #include "Macros.h"
 
 #include <ape_log.h>
@@ -143,7 +145,11 @@ void Context::onMessage(const SharedMessages::Message &msg)
         {
             const char *str = (char *)msg.dataPtr();
 
+#ifdef NIDUM_PRODUCT_FRONTEND
             Interface::SystemInterface::GetInstance()->print(str);
+#else
+            fwrite(str, 1, strlen(str), stdout);
+#endif
 
             free(msg.dataPtr());
         }
