3eb2a1b7ce80ba9ffa838cf94c7a460e892b6a63
efyx <n.trani@weelya.com>
2017-04-15 10:24:29 +0200

    Style

--- src/Embed/html5.js
+++ src/Embed/html5.js
@@ -5,266 +5,266 @@
 */
 
 {
-const Elements = require("./lib/Elements.js");
-const OS = require("OS");
-const IS_MOBILE = OS.platform == "ios" || OS.platform == "android";
+    const Elements = require("./lib/Elements.js");
+    const OS = require("OS");
+    const IS_MOBILE = OS.platform == "ios" || OS.platform == "android";
 
-function reportUnsupported(message) {
-    console.info("[HTML5Compat] " + message);
-}
-
-// {{{ Navigator
-var Navigator = (function() {
-    function PublicNavigator() {
-        throw new Error("Illegal constructor");
+    function reportUnsupported(message) {
+        console.info("[HTML5Compat] " + message);
     }
 
-    Object.defineProperties(PublicNavigator.prototype, {
-        "appCodeName": {
-            value: "Nidium",
-            writable: false
-        },
-        "appName": {
-            value: "Nidium",
-            writable: false
-        },
-        "appVersion": {
-            get: function() {
-                return __nidium__.version
-            },
-        },
-        "language": {
-            get: function() {
-                return (require("OS").language);
-            },
-        },
-        "platform": {
-            get: function() {
-                return (require("OS").platform);
-            },
-        },
-        "product": {
-            value: "Nidium",
-            writable: false
-        },
-        "userAgent": {
-            get: function() {
-                return this.appName + " / " + __nidium__.version +
-                   " (" + this.language + "; rv:" + __nidium__.build + ")" +
-                   IS_MOBILE ? "mobile" : "";
-            }
-        },
-    });
+    // {{{ Navigator
+    var Navigator = (function() {
+        function PublicNavigator() {
+            throw new Error("Illegal constructor");
+        }
 
-    PublicNavigator.prototype.vibrate = function() {
-        return true;
+        Object.defineProperties(PublicNavigator.prototype, {
+            "appCodeName": {
+                value: "Nidium",
+                writable: false
+            },
+            "appName": {
+                value: "Nidium",
+                writable: false
+            },
+            "appVersion": {
+                get: function() {
+                    return __nidium__.version
+                },
+            },
+            "language": {
+                get: function() {
+                    return (require("OS").language);
+                },
+            },
+            "platform": {
+                get: function() {
+                    return (require("OS").platform);
+                },
+            },
+            "product": {
+                value: "Nidium",
+                writable: false
+            },
+            "userAgent": {
+                get: function() {
+                    return this.appName + " / " + __nidium__.version +
+                       " (" + this.language + "; rv:" + __nidium__.build + ")" +
+                       IS_MOBILE ? "mobile" : "";
+                }
+            },
+        });
+
+        PublicNavigator.prototype.vibrate = function() {
+            return true;
+        };
+
+        var PrivateNavigator = function() {}
+        PrivateNavigator.prototype = PublicNavigator.prototype;
+
+        window.navigator = new PrivateNavigator();
+
+        return PublicNavigator;
+    })();
+    // }}}
+
+    // {{{ HTMLElement (Extending Canvas prototype)
+    Canvas.prototype.appendChild = function(child) {
+        if (child instanceof Canvas) {
+            this.add(child);
+        } else {
+            reportUnsupported("appendChild only supports canvas " + typeof child);
+        }
     };
 
-    var PrivateNavigator = function() {}
-    PrivateNavigator.prototype = PublicNavigator.prototype;
-
-    window.navigator = new PrivateNavigator();
-
-    return PublicNavigator;
-})();
-// }}}
-
-// {{{ HTMLElement (Extending Canvas prototype)
-Canvas.prototype.appendChild = function(child) {
-    if (child instanceof Canvas) {
-        this.add(child);
-    } else {
-        reportUnsupported("appendChild only supports canvas " + typeof child);
-    }
-};
-
-Object.defineProperty(Canvas.prototype, "parentNode", {
-    get: function() {
-        return this.getParent();
-    }
-});
-
-(function(){
-    const styles = new WeakMap();
-    const DEFAULTS = {
-        "userSelect": "auto",
-        "touchSelect": "auto",
-        "webkitTouchCallout": "auto",
-        "msContentZooming": "default",
-        "webkitUserDrag": "none",
-        "webkitTapHighlightColor": "black",
-    }
-
-    function getter(target, name) {
-        switch (name) {
-            case "width":
-            case "height":
-                return target[name];
-            default:
-                console.log("get", name);
-                if (name in DEFAULTS) {
-                    return DEFAULTS[name];
-                }
-
-                reportUnsupported("Unsupported " + name + " css property");
-                break;
+    Object.defineProperty(Canvas.prototype, "parentNode", {
+        get: function() {
+            return this.getParent();
         }
-    }
-    const styleHandler = {
-        has: function (target, name) {
-            return getter(target, name) ? true : false;
-        },
+    });
 
-        get: getter,
+    (function(){
+        const styles = new WeakMap();
+        const DEFAULTS = {
+            "userSelect": "auto",
+            "touchSelect": "auto",
+            "webkitTouchCallout": "auto",
+            "msContentZooming": "default",
+            "webkitUserDrag": "none",
+            "webkitTapHighlightColor": "black",
+        }
 
-        set: function(target, name, value) {
+        function getter(target, name) {
             switch (name) {
                 case "width":
                 case "height":
-                    target[name] = parseInt(value);
-                    break;
+                    return target[name];
                 default:
-                    reportUnsupported("Assignation of " + name + " css property is a no-op");
-                break;
-            }
+                    console.log("get", name);
+                    if (name in DEFAULTS) {
+                        return DEFAULTS[name];
+                    }
 
-            return true;
+                    reportUnsupported("Unsupported " + name + " css property");
+                    break;
+            }
+        }
+        const styleHandler = {
+            has: function (target, name) {
+                return getter(target, name) ? true : false;
+            },
+
+            get: getter,
+
+            set: function(target, name, value) {
+                switch (name) {
+                    case "width":
+                    case "height":
+                        target[name] = parseInt(value);
+                        break;
+                    default:
+                        reportUnsupported("Assignation of " + name + " css property is a no-op");
+                    break;
+                }
+
+                return true;
+            }
+        }
+
+        for (klass of [Canvas, Image]) {
+            Object.defineProperty(klass.prototype, "style", {
+                configurable: false,
+                get: function(target, name) {
+                    let style = styles.get(this);
+                    if (!style) {
+                        style = new Proxy(this, styleHandler);
+                        styles.set(this, style);
+                    }
+
+                    return style;
+                }
+            });
+        }
+    })();
+    //}}}
+
+    // {{{ Document
+    document.createElement = function(what) {
+        switch (what) {
+            case "canvas":
+                return new Canvas(window.innerWidth, window.innerHeight);
+
+            case "img":
+                return new Image();
+
+            default:
+                return Elements.Create(what);
         }
     }
 
-    for (klass of [Canvas, Image]) {
-        Object.defineProperty(klass.prototype, "style", {
-            configurable: false,
-            get: function(target, name) {
-                let style = styles.get(this);
-                if (!style) {
-                    style = new Proxy(this, styleHandler);
-                    styles.set(this, style);
-                }
+    document.createTextNode = function(text) {
+        return Elements.Create("textNode", text);
+    }
 
-                return style;
+    document.querySelector = function(sel) {
+        return document.getCanvasById(sel);
+    }
+
+    document.createElementNS = function(ns, what) {
+        return document.createElement(what);
+    }
+
+    document.addEventListener = function(name, fn) {
+        // XXX : Fixme, will not work for all events
+        document.canvas.addEventListener(name, fn);
+    }
+
+    document.body = document.canvas;
+    // }}}
+
+    // {{{ XMLHTTPRequest
+    var XMLHttpRequest = function() {
+        this.mimeType = "text/plain";
+        this.ev = {};
+        this.status = -1;
+        this.url = null;
+        this.responseType = "text";
+    }
+
+    XMLHttpRequest.prototype = {
+        overrideMimeType: function(type) {
+            this.mimeType = type;
+        },
+        setResponseType: function(type) {
+            this.responseType = type;
+        },
+        addEventListener: function(name, fn) {
+            if (!this.ev[name]) this.ev[name] = [];
+
+            this.ev[name].push(fn);
+        },
+        _fireEvent: function(name, ev) {
+            if (!this.ev[name]) return;
+
+            for (fn of this.ev[name]) {
+                fn.call(this, ev);
             }
-        });
-    }
-})();
-//}}}
+        },
+        send: function() {
+            if (!this.url) return;
 
-// {{{ Document
-document.createElement = function(what) {
-    switch (what) {
-        case "canvas":
-            return new Canvas(window.innerWidth, window.innerHeight);
+            if (this.responseType == "text" || this.responseType == "arraybuffer") {
+                const opt = {};
+                if (this.responseType == "text") opt.encoding = "utf8";
+                File.read(this.url, opt, function(err, data) {
+                    if (err) {
+                        this._fireEvent("error", err);
+                        return;
+                    }
 
-        case "img":
-            return new Image();
+                    this.status = 200;
 
-        default:
-            return Elements.Create(what);
-    }
-}
-
-document.createTextNode = function(text) {
-    return Elements.Create("textNode", text);
-}
-
-document.querySelector = function(sel) {
-    return document.getCanvasById(sel);
-}
-
-document.createElementNS = function(ns, what) {
-    return document.createElement(what);
-}
-
-document.addEventListener = function(name, fn) {
-    // XXX : Fixme, will not work for all events
-    document.canvas.addEventListener(name, fn);
-}
-
-document.body = document.canvas;
-// }}}
-
-// {{{ XMLHTTPRequest
-var XMLHttpRequest = function() {
-    this.mimeType = "text/plain";
-    this.ev = {};
-    this.status = -1;
-    this.url = null;
-    this.responseType = "text";
-}
-
-XMLHttpRequest.prototype = {
-    overrideMimeType: function(type) {
-        this.mimeType = type;
-    },
-    setResponseType: function(type) {
-        this.responseType = type;
-    },
-    addEventListener: function(name, fn) {
-        if (!this.ev[name]) this.ev[name] = [];
-
-        this.ev[name].push(fn);
-    },
-    _fireEvent: function(name, ev) {
-        if (!this.ev[name]) return;
-
-        for (fn of this.ev[name]) {
-            fn.call(this, ev);
-        }
-    },
-    send: function() {
-        if (!this.url) return;
-
-        if (this.responseType == "text" || this.responseType == "arraybuffer") {
-            const opt = {};
-            if (this.responseType == "text") opt.encoding = "utf8";
-            File.read(this.url, opt, function(err, data) {
-                if (err) {
-                    this._fireEvent("error", err);
-                    return;
-                }
-
+                    this._fireEvent("load", {target: {response: data}});
+                }.bind(this));
+            } else if (this.responseType == "blob") {
+                // FIXME : This is probably wrong, it's just a workaround
+                // to quickly make three.js works inside Nidium.
+                // For a more general approach we should wrap an
+                // an ArrayBuffer inside a Blob.
+                const f = new File(this.url);
                 this.status = 200;
-
-                this._fireEvent("load", {target: {response: data}});
-            }.bind(this));
-        } else if (this.responseType == "blob") {
-            // FIXME : This is probably wrong, it's just a workaround
-            // to quickly make three.js works inside Nidium.
-            // For a more general approach we should wrap an
-            // an ArrayBuffer inside a Blob.
-            const f = new File(this.url);
-            this.status = 200;
-            this._fireEvent("load", {target: {response: f}});
+                this._fireEvent("load", {target: {response: f}});
+            }
+        },
+        open: function(method, url) {
+            this.method = method;
+            this.url = url;
         }
-    },
-    open: function(method, url) {
-        this.method = method;
-        this.url = url;
     }
-}
-// }}}
+    // }}}
 
-// {{{ Blob
-var URL = {
-    _map: new Map(),
-    createObjectURL: function(blob) {
-        URL._map.set(blob, true);
-        return blob;
-    },
-    revokeObjectURL: function(obj) {
-        URL._map.delete(obj);
+    // {{{ Blob
+    var URL = {
+        _map: new Map(),
+        createObjectURL: function(blob) {
+            URL._map.set(blob, true);
+            return blob;
+        },
+        revokeObjectURL: function(obj) {
+            URL._map.delete(obj);
+        }
     }
-}
-// }}}
+    // }}}
 
-// {{{ Window
-// Some basic event forwarding
-for (let name of ["touchstart", "touchmove", "touchend"]) {
-    document.canvas.addEventListener(name, function(name, ev) {
-        window.fireEvent(name, ev);
-    }.bind(window, name));
+    // {{{ Window
+    // Some basic event forwarding
+    for (let name of ["touchstart", "touchmove", "touchend"]) {
+        document.canvas.addEventListener(name, function(name, ev) {
+            window.fireEvent(name, ev);
+        }.bind(window, name));
 
-    window["on" + name] = null;
-}
-// }}}
+        window["on" + name] = null;
+    }
+    // }}}
 }
