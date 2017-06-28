5f57812dedbd457ffac8f6dbc5aa3e113bb3dc86
Anthony Catel <paraboul@gmail.com>
2017-06-28 16:58:34 +0200

    Core::Utils::GetTick now uses APE_get_monotonic_time()

--- src/Core/Utils.cpp
+++ src/Core/Utils.cpp
@@ -15,6 +15,7 @@
 #include <ape_sha1.h>
 #include <ape_base64.h>
 #include <ape_blowfish.h>
+#include <ape_timers_next.c>
 
 namespace Nidium {
 namespace Core {
@@ -30,98 +31,11 @@ namespace Core {
 #else
 #include <time.h>
 #include <arpa/inet.h>
-
-#ifdef __WIN32
-LARGE_INTEGER
-getFILETIMEoffset()
-{
-    SYSTEMTIME s;
-    FILETIME f;
-    LARGE_INTEGER t;
-
-    s.wYear         = 1970;
-    s.wMonth        = 1;
-    s.wDay          = 1;
-    s.wHour         = 0;
-    s.wMinute       = 0;
-    s.wSecond       = 0;
-    s.wMilliseconds = 0;
-    SystemTimeToFileTime(&s, &f);
-    t.QuadPart = f.dwHighDateTime;
-    t.QuadPart <<= 32;
-    t.QuadPart |= f.dwLowDateTime;
-    return (t);
-}
-
-int clock_gettime(int X, struct timeval *tv)
-{
-    LARGE_INTEGER t;
-    FILETIME f;
-    double microseconds;
-    static LARGE_INTEGER offset;
-    static double frequencyToMicroseconds;
-    static int initialized            = 0;
-    static BOOL usePerformanceCounter = 0;
-
-    if (!initialized) {
-        LARGE_INTEGER performanceFrequency;
-        initialized = 1;
-        usePerformanceCounter
-            = QueryPerformanceFrequency(&performanceFrequency);
-        if (usePerformanceCounter) {
-            QueryPerformanceCounter(&offset);
-            frequencyToMicroseconds
-                = static_cast<double>(performanceFrequency.QuadPart) / 1000000.;
-        } else {
-            offset                  = getFILETIMEoffset();
-            frequencyToMicroseconds = 10.;
-        }
-    }
-    if (usePerformanceCounter)
-        QueryPerformanceCounter(&t);
-    else {
-        GetSystemTimeAsFileTime(&f);
-        t.QuadPart = f.dwHighDateTime;
-        t.QuadPart <<= 32;
-        t.QuadPart |= f.dwLowDateTime;
-    }
-
-    t.QuadPart -= offset.QuadPart;
-    microseconds = static_cast<double>(t.QuadPart) / frequencyToMicroseconds;
-    t.QuadPart   = microseconds;
-    tv->tv_sec   = t.QuadPart / 1000000;
-    tv->tv_usec = t.QuadPart % 1000000;
-    return (0);
-}
-static __inline uint64_t mach_absolute_time()
-{
-    struct timeval t;
-    clock_gettime(0, &t);
-
-    return ((uint64_t)t.tv_sec * 1000000 + (uint64_t)t.tv_usec) * 1000;
-}
-#else // !__WIN32
-static __inline uint64_t mach_absolute_time()
-{
-    struct timespec t;
-#ifdef CLOCK_MONOTONIC_RAW
-#define USED_CLOCK CLOCK_MONOTONIC_RAW
-#else
-#define USED_CLOCK CLOCK_MONOTONIC
-#endif
-    clock_gettime(USED_CLOCK, &t);
-
-    return static_cast<uint64_t>(t.tv_sec) * 1000000000
-           + static_cast<uint64_t>(t.tv_nsec);
-}
-#endif
 #endif
 
-/* TODO: iOS :
- * http://shiftedbits.org/2008/10/01/mach_absolute_time-on-the-iphone/ */
 uint64_t Utils::GetTick(bool ms)
 {
-    return mach_absolute_time() / (ms ? 1000000LL : 1LL);
+    return APE_get_monotonic_time(NULL, NULL, NULL) / (ms ? 1000000LL : 1LL);
 }
 
 static const char *week[]   = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
