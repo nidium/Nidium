c5e9f018094c51e99e5800db4220f9f2b7b0bbdd
Anthony Catel <paraboul@gmail.com>
2017-06-30 18:01:31 +0200

    Fix crash on refresh

--- src/Frontend/Context.cpp
+++ src/Frontend/Context.cpp
@@ -825,6 +825,9 @@ JSObject *Context::ReadStructuredCloneOp(JSContext *cx,
 
 Context::~Context()
 {
+
+    m_ContextCache.emptyCache();
+
     if (m_DebugHandler != NULL) {
         delete m_DebugHandler->getContext();
         delete m_DebugHandler;
